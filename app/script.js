fe29fe55fdfd5e6ca6470feb38f10518fe73b7fb
LukasGX <lukas@grambs.info>
2026-03-04 15:55:43 +0100

    pwa ready

--- app/script.js
+++ app/script.js
@@ -274,3 +274,12 @@ function startCountdown() {
 
 // Start
 startCountdown();
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("/sw.js")
+			.then((registration) => {})
+			.catch((error) => {});
+	});
+}
